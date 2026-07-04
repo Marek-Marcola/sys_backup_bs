af4f3c8f02a6ba5f14dd309f2accc2cad9463ff1
Marek Marcola <Marek.Marcola@gmail.com>
2026-07-05 01:52:00 +0200

    bsync.sh: add LINK stage
    Change-Id: I3f8006ec90044f146817b2091ac7daa5780db71e

--- bsync.sh
+++ bsync.sh
@@ -1,6 +1,174 @@
 #!/bin/bash
 
-VERSION_BIN="260614"
+VERSION_BIN="260705"
 
 SN="${0##*/}"
 ID="[$SN]"
+
+INSTALL_RSYNC=0
+INSTALL_ANPB=0
+INSTALL_ANPB_HP="bs"
+VERSION=0
+STAGE_LIST=0
+LINK=0
+EVAL=0
+HELP=0
+QUIET=0
+
+s=0
+
+: ${A:=${SN%.sh}}
+: ${APN:=$(echo $A|cut -d- -f2)}
+: ${API:=$(echo $A|cut -d- -f3-)}
+: ${LDIR:="/usr/local/bin/alias-bs"}
+: ${COMM:=$(readlink -f ${BASH_SOURCE})}
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    --ver*|-ver*)
+      VERSION=1
+      shift
+      ;;
+    --inst*|-inst*)
+      INSTALL_RSYNC=1
+      shift
+      ;;
+    --anpb|-anpb)
+      INSTALL_ANPB=1
+      [[ -n "$2" && ${2:0:1} != "-" ]] && INSTALL_ANPB_HP="$2" && shift
+      shift
+      ;;
+    --stage|-stage)
+      STAGE_LIST=1
+      shift
+      ;;
+    -A)
+      A="$2"
+      APN=$(echo $A|cut -d- -f2)
+      API=$(echo $A|cut -d- -f3-)
+      ID="[$A]"
+      shift; shift
+      ;;
+    -L)
+      LINK=1
+      shift
+      ;;
+    -x)
+      EVAL=1
+      shift
+      ;;
+    -h|-help|--help)
+      HELP=1
+      shift
+      ;;
+    -v)
+      VERB=1
+      shift
+      ;;
+    -q)
+      QUIET=1
+      shift
+      ;;
+    *)
+      ARGS1+=("$1")
+      shift
+      ;;
+  esac
+done
+
+#
+# stage: HELP
+#
+if [ $HELP -eq 1 ]; then
+  echo "$SN -ver                      # version"
+  echo "$SN -inst [-x]                # install with rsync"
+  echo "$SN -anpb [host_pattern] [-x] # install with ansible"
+  echo "$SN -stage                    # stage list"
+  echo ""
+  echo "$SN -L [-x]                   # link show,run"
+  echo ""
+  echo "$SN -l                        # list backup"
+  echo "$SN -ls                       # list system"
+  echo "$SN                           # info"
+  exit 0
+fi
+
+#
+# stage: CONFIG
+#
+: ${EDIR=/usr/local/etc/bsync.d}
+: ${BID=$(hostname -s)}
+
+if [ -f $(dirname $EDIR)/bsync.env ]; then
+  . $(dirname $EDIR)/bsync.env
+  EFILE=$(dirname $EDIR)/bsync.env
+fi
+
+#
+# stage: VERSION
+#
+if [ $VERSION -eq 1 ]; then
+  echo "${0##*/}  $VERSION_BIN"
+  [[ "$VERSION_ENV" != "" ]] && echo "bsync.env $VERSION_ENV"
+  exit 0
+fi
+
+#
+# stage: STAGE-LIST
+#
+if [ $STAGE_LIST -eq 1 ]; then
+  cat $COMM | grep '^#' | grep 'stage:'
+  exit 0
+fi
+
+#
+# stage: INFO
+#
+if [ $QUIET -eq 0 ]; then
+  (( $s != 0 )) && echo; ((++s))
+  echo "$ID: stage: INFO"
+
+  echo "efile  = ${EFILE:-[none]}"
+  echo "App    = ${A:-[none]}"
+  echo "APN    = ${APN:-[none]}"
+  echo "API    = ${API:-[none]}"
+  echo "ldir   = $LDIR"
+fi
+
+#
+# stage: LINK
+#
+if [ $LINK -ne 0 ]; then
+  (( $s != 0 )) && echo; ((++s))
+  echo "$ID: stage: LINK"
+
+  if [ ! -d $EDIR ]; then
+    echo $ID: directory not found: $EDIR
+    exit 1
+  fi
+  if [ ! -d $LDIR ]; then
+    echo $ID: directory not found: $LDIR
+    exit 1
+  fi
+
+  ls $EDIR/ | \
+  while read E; do
+    if [ -x $EDIR/$E ]; then
+      continue
+    fi
+
+    LSRC=$COMM
+
+    if [ ! -f $LDIR/$E ]; then
+      if [ $EVAL -ne 0 ]; then
+        set -ex
+        ln -svr $LSRC $LDIR/$E
+        { set +ex; } 2>/dev/null
+      else
+        echo "ln -svr $LSRC $LDIR/$E"
+      fi
+    else
+      echo "# ln -svr $LSRC $LDIR/$E"
+    fi
+  done
+fi
