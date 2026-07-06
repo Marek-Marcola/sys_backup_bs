afddce99be1fb95278f9af028401954cb0c1f5b6
Marek Marcola <Marek.Marcola@gmail.com>
2026-07-07 00:46:16 +0200

    bsync.sh: add ENV-EDIT,ENV-SHOW stages
    Change-Id: I9e4e4e6ee28545e83ba0a99ce9550339c4626b77

--- bsync.sh
+++ bsync.sh
@@ -12,6 +12,9 @@ VERSION=0
 STAGE_LIST=0
 LINK=0
 BACKUP=0
+ESHOW=0
+ESHOW_RE=""
+EEDIT=0
 EVAL=0
 HELP=0
 QUIET=0
@@ -54,6 +57,16 @@ while [ $# -gt 0 ]; do
       LINK=1
       shift
       ;;
+    -s)
+      ESHOW=1
+      ESHOW_RE="$2"
+      QUIET=1
+      shift
+      ;;
+    -E)
+      EEDIT=1
+      shift
+      ;;
     -B)
       BACKUP=1
       shift
@@ -91,6 +104,9 @@ if [ $HELP -eq 1 ]; then
   echo "$SN -stage                    # stage list"
   echo ""
   echo "$SN -L [-x]                   # link show,exec"
+  echo ""
+  echo "$SN -s [re]                   # env show"
+  echo "$SN -E                        # env edit"
   echo ""
   echo "$SN -B [-x]                   # backup test,exec"
   echo ""
@@ -268,4 +284,53 @@ if [ $BACKUP -ne 0 ]; then
     { set +ex; } 2>/dev/null
     echo
   done
+fi
+
+#
+# stage: ENV-SHOW
+#
+if [ $ESHOW -eq 1 ]; then
+  (( $s != 0 )) && echo; ((++s))
+  echo "$ID: stage: ENV-SHOW (rexp: *$ESHOW_RE*)"
+
+  if [ "$A" != "bsync" -a  "$ESHOW_RE" = "" ]; then
+    if [ ! -f $EDIR/$A ]; then
+      echo file not found: $EDIR/$A
+    else
+      set -ex
+      cat $EDIR/$A
+      { set +ex; } 2>/dev/null
+    fi
+  else
+    for f in $EDIR/*$ESHOW_RE*; do
+      if [ -f $f ]; then
+        set -ex
+        cat $f  2>&1
+        { set +ex; } 2>/dev/null
+        echo
+      fi
+    done
+  fi
+fi
+
+#
+# stage: ENV-EDIT
+#
+if [ $EEDIT -eq 1 ]; then
+  (( $s != 0 )) && echo; ((++s))
+  echo "$ID: stage: ENV-EDIT"
+
+  if [ ! -d $EDIR ]; then
+    echo directory not found: $EDIR
+  else
+    if [ "$EDITOR" != "" ]; then
+      set -ex
+      $EDITOR $EDIR/$A
+      { set +ex; } 2>/dev/null
+    else
+      set -ex
+      vi $EDIR/$A
+      { set +ex; } 2>/dev/null
+    fi
+  fi
 fi
