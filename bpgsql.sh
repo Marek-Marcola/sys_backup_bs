a9705a9b142ef8556d721b2d6e54666a16694f97
Marek Marcola <Marek.Marcola@gmail.com>
2026-07-07 15:26:12 +0200

    bpgsql.sh: simplify SPOOLER-LIST stage
    Change-Id: I18a30ffadd159acdd0fcefd538679c76e1950a4a

--- bpgsql.sh
+++ bpgsql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION_BIN="260706"
+VERSION_BIN="260707"
 
 SN="${0##*/}"
 ID="[$SN]"
@@ -552,14 +552,14 @@ if [ $BACKUP_ONLINE_LIST -ne 0 ]; then
 
   if [ "$A" != "bpgsql" ]; then
     set -x
-    tree --noreport -F -h -C -L 2 -l -I perm $ADIR
+    tree --noreport -F -C -L 2 -l -I perm $ADIR
     { set +x; } 2>/dev/null
     if [ -d $PDIR ]; then
-      tree --noreport -F -h -C -L 2 -l -I perm $PDIR
+      tree --noreport -F -C -L 2 -l -I perm $PDIR
     fi
   else
     set -x
-    tree --noreport -F -h -C -l $BADIR
+    tree --noreport -F -C -l $BADIR
     { set +x; } 2>/dev/null
   fi
 fi
