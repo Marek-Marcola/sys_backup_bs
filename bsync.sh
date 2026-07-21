bc99e5209e86fe7736b1c43db8ee4694af47bbe6
Marek Marcola <Marek.Marcola@gmail.com>
2026-07-21 13:47:53 +0200

    bsync.sh: improve help
    Change-Id: Ibbca9fed64a366821d0d78df3310c636a6f276f8

--- bsync.sh
+++ bsync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION_BIN="260717"
+VERSION_BIN="260721"
 
 SN="${0##*/}"
 ID="[$SN]"
@@ -116,6 +116,8 @@ done
 # stage: HELP
 #
 if [ $HELP -eq 1 ]; then
+  echo "Backup filesystem (rsync)."
+  echo ""
   echo "$SN -ver                      # version"
   echo "$SN -inst [-x]                # install with rsync"
   echo "$SN -anpb [host_pattern] [-x] # install with ansible"
